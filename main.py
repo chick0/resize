e39a4177c305f4580e0835f31484073117d5a8dd
chick_0 <chick_0@ch1ck.xyz>
2022-09-14 19:24:51 +0900

    feat: Image resize to 1920x?

---
+++ main.py
@@ -0,0 +1,39 @@
+from os import mkdir
+from os import listdir
+from os.path import join
+from os.path import exists
+from PIL import Image
+
+INPUT = join(".", "input")
+OUTPUT = join(".", "output")
+
+
+def test_dir() -> None:
+    for target in [INPUT, OUTPUT]:
+        if not exists(target):
+            mkdir(target)
+
+
+def resize() -> None:
+    for file in listdir(INPUT):
+        print(file)
+        image = Image.open(join(INPUT, file))
+        width, height = image.size
+
+        P = 1920
+        if width >= P:
+            q = height / (width / P)
+            q = height - q
+
+            height = height - q
+            width = P
+
+            image = image.resize((width, int(height)))
+
+        name: str = file.rsplit(".", 1)[0]
+        image.save(join(OUTPUT, name + ".png"))
+
+
+if __name__ == "__main__":
+    test_dir()
+    resize()
